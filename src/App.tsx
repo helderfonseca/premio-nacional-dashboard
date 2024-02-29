e3ec5fa7985ab29057516413b11b915278903a80
Helder Fonseca <helder2391@gmail.com>
2024-02-29 16:28:08 -0100

    change card's position.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.module.scss'
 import Container from 'react-bootstrap/Container';
 import classes from './App.module.scss';
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 import { BarChart } from "./components/BarChart";
 import { faker } from '@faker-js/faker';
 import { 
@@ -52,107 +52,125 @@ const data: Data = {
 
 function App() {
     return (
-        <Container fluid className={classes.content}>
-            <Card style={{ width: '20rem' }}>
-            <Card.Body>
-                <Card.Title style={{ fontWeight: 700 }}>Alimentação</Card.Title>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Card.Text style={{ fontSize: '13px' }}>Pastilim de dona Graça</Card.Text>
-                    <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Card.Text style={{ fontSize: '13px' }}>Bodji Sab Sab</Card.Text>
-                    <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Card.Text style={{ fontSize: '13px' }}>Gigalanche</Card.Text>
-                    <Card.Text style={{ fontWeight: 700 }}>320</Card.Text>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Card.Text style={{ fontSize: '13px' }}>Pinga Djond`Elena</Card.Text>
-                    <Card.Text style={{ fontWeight: 700 }}>320</Card.Text>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Card.Text style={{ fontSize: '13px' }}>Atleta Kriol Lanchonete</Card.Text>
-                    <Card.Text style={{ fontWeight: 700 }}>320</Card.Text>
-                </div>
-            </Card.Body>
-            </Card>
-            <Card style={{ width: '20rem' }}>
-                <Card.Body>
-                    <Card.Title style={{ fontWeight: 700 }}>Arte</Card.Title>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Nharti Artesanato e Decoração</Card.Text>
-                        <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Sonia Artes</Card.Text>
-                        <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
-                    </div>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '20rem' }}>
-                <Card.Body>
-                    <Card.Title style={{ fontWeight: 700 }}>Estética/Beleza</Card.Title>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Kuale Miniz</Card.Text>
-                        <Card.Text style={{ fontWeight: 700, background: 'brown', borderRadius: '50%', padding: '5px', color: '#fff' }}>523</Card.Text>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>By Linda Rocha Beauty</Card.Text>
-                        <Card.Text style={{ fontWeight: 700, background: 'brown', borderRadius: '50%', padding: '5px', color: '#fff' }}>520</Card.Text>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Bell Make Up</Card.Text>
-                        <Card.Text style={{ fontWeight: 700, background: 'brown', borderRadius: '50%', padding: '5px', color: '#fff' }}>320</Card.Text>
-                    </div>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '20rem' }}>
-                <Card.Body>
-                    <Card.Title style={{ fontWeight: 700 }}>Investimento</Card.Title>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Chave Group Socidade Unipossol</Card.Text>
-                        <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Bodjy Sab Sab</Card.Text>
-                        <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Bodjy Sab Sab</Card.Text>
-                        <Card.Text style={{ fontWeight: 700 }}>320</Card.Text>
-                    </div>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '20rem' }}>
-                <Card.Body>
-                    <Card.Title style={{ fontWeight: 700 }}>Saúde</Card.Title>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Basefightclub</Card.Text>
-                        <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Lidie Clínica Dentária sociedade unipessoal lda</Card.Text>
-                        <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
-                    </div>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '20rem' }}>
-                <Card.Body>
-                    <Card.Title style={{ fontWeight: 700 }}>Serviço</Card.Title>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>Telma Pires Serviços Comerciais</Card.Text>
-                        <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Card.Text style={{ fontSize: '13px' }}>ED - Comercio & Prestação de Serviço, Lda</Card.Text>
-                        <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
-                    </div>
-                </Card.Body>
-            </Card>
-
-            <BarChart chartData={data} />
+        <Container className={classes.content}>
+            <Row xs={1} md={3} className="g-4 justify-content-center">
+                <Col className='d-flex justify-content-between align-items-stretch'>
+                    <Card style={{ width: '100%'}}>
+                    <Card.Body>
+                        <Card.Title style={{ fontWeight: 700 }}>Alimentação</Card.Title>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Card.Text style={{ fontSize: '13px' }}>Pastilim de dona Graça</Card.Text>
+                            <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Card.Text style={{ fontSize: '13px' }}>Bodji Sab Sab</Card.Text>
+                            <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Card.Text style={{ fontSize: '13px' }}>Gigalanche</Card.Text>
+                            <Card.Text style={{ fontWeight: 700 }}>320</Card.Text>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Card.Text style={{ fontSize: '13px' }}>Pinga Djond`Elena</Card.Text>
+                            <Card.Text style={{ fontWeight: 700 }}>320</Card.Text>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Card.Text style={{ fontSize: '13px' }}>Atleta Kriol Lanchonete</Card.Text>
+                            <Card.Text style={{ fontWeight: 700 }}>320</Card.Text>
+                        </div>
+                    </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='d-flex justify-content-between align-items-stretch'>
+                    <Card style={{ width: '100%'}}>
+                        <Card.Body>
+                            <Card.Title style={{ fontWeight: 700 }}>Arte</Card.Title>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Nharti Artesanato e Decoração</Card.Text>
+                                <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Sonia Artes</Card.Text>
+                                <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='d-flex justify-content-between align-items-stretch'>
+                    <Card style={{ width: '100%'}}>
+                        <Card.Body>
+                            <Card.Title style={{ fontWeight: 700 }}>Estética/Beleza</Card.Title>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Kuale Miniz</Card.Text>
+                                <Card.Text style={{ fontWeight: 700, background: 'brown', borderRadius: '50%', padding: '5px', color: '#fff' }}>523</Card.Text>
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>By Linda Rocha Beauty</Card.Text>
+                                <Card.Text style={{ fontWeight: 700, background: 'brown', borderRadius: '50%', padding: '5px', color: '#fff' }}>520</Card.Text>
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Bell Make Up</Card.Text>
+                                <Card.Text style={{ fontWeight: 700, background: 'brown', borderRadius: '50%', padding: '5px', color: '#fff' }}>320</Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='d-flex justify-content-between align-items-stretch'>
+                    <Card style={{ width: '100%'}}>
+                        <Card.Body>
+                            <Card.Title style={{ fontWeight: 700 }}>Investimento</Card.Title>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Chave Group Socidade Unipossol</Card.Text>
+                                <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Bodjy Sab Sab</Card.Text>
+                                <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Bodjy Sab Sab</Card.Text>
+                                <Card.Text style={{ fontWeight: 700 }}>320</Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='d-flex justify-content-between align-items-stretch'>
+                    <Card style={{ width: '100%'}}>
+                        <Card.Body>
+                            <Card.Title style={{ fontWeight: 700 }}>Saúde</Card.Title>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Basefightclub</Card.Text>
+                                <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Lidie Clínica Dentária sociedade unipessoal lda</Card.Text>
+                                <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='d-flex justify-content-between align-items-stretch'>
+                    <Card style={{ width: '100%'}}>
+                        <Card.Body>
+                            <Card.Title style={{ fontWeight: 700 }}>Serviço</Card.Title>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>Telma Pires Serviços Comerciais</Card.Text>
+                                <Card.Text style={{ fontWeight: 700 }}>123</Card.Text>
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Card.Text style={{ fontSize: '13px' }}>ED - Comercio & Prestação de Serviço, Lda</Card.Text>
+                                <Card.Text style={{ fontWeight: 700 }}>520</Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row xs={1} md={2} className='justify-content-center'>
+                <Col>
+                    <BarChart chartData={data} />
+                </Col>
+            </Row>
+            
         </Container>
 )
 }
